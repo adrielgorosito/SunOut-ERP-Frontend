450b9f228eddb404495f1f1f4e8aa2425172cb8c
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-03-10 17:42:10 -0300

    Init auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './core/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthComponent } from './core/auth/auth.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
