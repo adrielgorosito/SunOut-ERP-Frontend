2006f7dc8054a1ba7ed30092538173bfbf0135b7
Adriel Gorosito <adrielgorosito14@gmail.com>
2024-03-14 00:09:09 -0300

    Added styles to login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './core/auth/auth.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -24,6 +24,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatMenuModule,
     MatCardModule,
     FormsModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
--- src/app/core/auth/auth.component.ts
+++ src/app/core/auth/auth.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {
 
+export class AuthComponent {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
+  
+  hide = true;
+
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  protected submit() {
+    if (this.loginForm.invalid) this.router.navigate(['/login']);
+    console.log("Logged in!");
+  }
 }
