e27cec95445b26e55d55c28da58846e2d2db6209
Banti <santiagobotali@gmail.com>
2024-03-26 22:17:47 -0300

    New features. Imports and admin path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './core/auth/auth.component';
+import { AdminComponent } from './features/admin/admin.component';
 
 const routes: Routes = [
   { path: 'login', component: AuthComponent },
+  { path: 'admin', component: AdminComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,22 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar'; 
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { SideNavComponent } from './core/layout/side-nav/side-nav.component';
+import { AdminComponent } from './features/admin/admin.component'; 
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
+    HeaderComponent,
+    SideNavComponent,
+    AdminComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,6 +43,10 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatIconModule,
     MatButtonModule,
     MatSnackBarModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
